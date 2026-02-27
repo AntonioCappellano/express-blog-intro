bef835e805d92592292e0e2c11b560045c32eeee
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-27 14:33:05 +0100

    Refactor root route and add blog list endpoint

--- app.js
+++ app.js
@@ -6,10 +6,48 @@ const appUrl = `http://localhost:${port}/`;
 app.get("/", (req, res) => {
   const responseData = {
     message: "Benvenuti nel mio Blog",
-    success: true
-  }
+    success: true,
+  };
 
-  res.status(200).json(responseData)
+  res.status(200).json(responseData);
+});
+
+app.get("/bacheca", (req, res) => {
+  const blogList = [
+    {
+      title: "ciambellone",
+      content: "ciambellone molto buono",
+      image: "",
+      tags: ["soffice", "leggero"],
+    },
+
+    {
+      title: "cracker barbabietola",
+      content: "cracker croccanti alla barbabietola",
+      image: "",
+      tags: ["salato", "snacks"],
+    },
+    {
+      title: "panefritto dolce",
+      content: "fette di pane fritto dorate",
+      image: "",
+      tags: ["dolce", "fritto"],
+    },
+    {
+      title: "pasta barbabietola",
+      content: "pasta fresca dal colore vivace",
+      image: "",
+      tags: ["primo", "barbabietola"],
+    },
+    {
+      title: "torta paesana",
+      content: "dolce rustico",
+      image: "",
+      tags: ["dolce", "tradizionale"],
+    },
+  ];
+
+  res.json(blogList);
 });
 
 app.listen(port, () => {

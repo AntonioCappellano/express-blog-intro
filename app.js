4f4a234b58ba41bd66bf93911c2f0e9a6525de6f
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-27 14:59:33 +0100

    add static asset and path image

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const appUrl = `http://localhost:${port}/`;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   const responseData = {
     message: "Benvenuti nel mio Blog",
@@ -12,41 +14,42 @@ app.get("/", (req, res) => {
   res.status(200).json(responseData);
 });
 
-app.get("/bacheca", (req, res) => {
-  const blogList = [
+
+const blogList = [
     {
       title: "ciambellone",
       content: "ciambellone molto buono",
-      image: "",
+      image: "/images/ciambellone.jpeg",
       tags: ["soffice", "leggero"],
     },
 
     {
       title: "cracker barbabietola",
       content: "cracker croccanti alla barbabietola",
-      image: "",
+      image: "/images/cracker_barbabietola.jpeg",
       tags: ["salato", "snacks"],
     },
     {
       title: "panefritto dolce",
       content: "fette di pane fritto dorate",
-      image: "",
+      image: "/images/pane_fritto_dolce.jpeg",
       tags: ["dolce", "fritto"],
     },
     {
       title: "pasta barbabietola",
       content: "pasta fresca dal colore vivace",
-      image: "",
+      image: "/images/pasta_barbabietola.jpeg",
       tags: ["primo", "barbabietola"],
     },
     {
       title: "torta paesana",
       content: "dolce rustico",
-      image: "",
+      image: "/images/torta_paesana.jpeg",
       tags: ["dolce", "tradizionale"],
     },
   ];
 
+app.get("/bacheca", (req, res) => {
   res.json(blogList);
 });
 
